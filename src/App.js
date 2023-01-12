0ac52b883c4d9d4c9ec00e5192cb6868df890aa3
Anshika Gupta <63110987+Anshika66-Gupta@users.noreply.github.com>
2023-01-12 21:53:43 +0530

    Day-1

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import "./App.css";
+import React from "react";
+ class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: "",
+      },
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now(),
+      },
+    });
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input
+              type="text"
+              placeholder="Enter Text"
+              value={this.state.currentItem.string}
+              onChange={this.handleInput}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
